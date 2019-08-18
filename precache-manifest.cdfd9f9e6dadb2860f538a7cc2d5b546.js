14a71510544e493ce239756b58a2c7239244fc7e
vinchi777 <judecalimbas@gmail.com>
2019-08-18 20:49:05 +0800

    Updates

---
+++ precache-manifest.cdfd9f9e6dadb2860f538a7cc2d5b546.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d68b04a0c2c39b3f2eda450b82d8d00",
+    "url": "/pokemon-react-play/index.html"
+  },
+  {
+    "revision": "54cf992412ad7f25b91d",
+    "url": "/pokemon-react-play/static/css/2.e8d046a2.chunk.css"
+  },
+  {
+    "revision": "e5feac75fab76767dfca",
+    "url": "/pokemon-react-play/static/css/main.e4c4d948.chunk.css"
+  },
+  {
+    "revision": "54cf992412ad7f25b91d",
+    "url": "/pokemon-react-play/static/js/2.2af414fa.chunk.js"
+  },
+  {
+    "revision": "e5feac75fab76767dfca",
+    "url": "/pokemon-react-play/static/js/main.0eea7f85.chunk.js"
+  },
+  {
+    "revision": "a4f844c0aa8275ba4405",
+    "url": "/pokemon-react-play/static/js/runtime~main.877a98b9.js"
+  }
+]);
